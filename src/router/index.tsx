8a44661764a50f05d1cc8330ae46859d330476db
David Huertas <davidhuertasortiz@gmail.com>
2024-02-24 14:07:22 +0100

    feat(KNL-7): Add new pages to router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,22 @@
 import { Route, Switch } from 'wouter'
 
 import Index from '../pages'
+import Dashboard from '../pages/Dashboard'
 import NotFound from '../pages/NotFound'
+import Profile from '../pages/Profile'
+import Search from '../pages/Search'
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
 
 export default function Router() {
   return (
     <Switch>
       <Route path='/' component={Index} />
-      <Route path='/dashboard' />
-      <Route path='/signup' />
-      <Route path='/signin' />
-      <Route path='/dashboard' />
-      <Route path='/search' />
-      <Route path='/search/profile/:id' />
+      <Route path='/dashboard' component={Dashboard} />
+      <Route path='/signup' component={SignUp} />
+      <Route path='/signin' component={SignIn} />
+      <Route path='/search' component={Search} />
+      <Route path='/search/profile/:id' component={Profile} />
       <Route>
         <NotFound />
       </Route>
